3616de35c607522f380454fc55e1882725da033d
Tagoregit <tagorelavudya0.com>
2024-12-06 10:06:59 +0530

    loop

---
+++ index.js
@@ -0,0 +1,197 @@
+// 8. What is the result of the following code?
+var i = 0;
+for (; i < 3;) {
+    console.log(i);
+    i++;
+}
+// 9. What will this code print?
+var i = 1;
+do {
+    console.log(i);
+    i++;
+} while (i < 4);
+// 10. What will be the output of the following code?
+var i = 0;
+for (i = 0; i < 5; i++) {
+    if (i == 2) {
+        break;
+    }
+    console.log(i);
+}
+console.clear()
+// 1. What will be the output of the following code?
+var i = 0;
+for (i = 0; i < 3; i++) {
+    console.log(i);
+}
+// 2. What will the following code output?
+var i = 0;
+while (i < 2) {
+    console.log(i);
+    i++;
+}
+console.clear()
+// 3. How many times will the loop execute?
+var i = 0;
+while (i < 5) {
+    i++;
+}
+console.log(i)
+// 4. What will this code print?
+for (var i = 0; i <= 3; i++) {
+    console.log(i);
+}
+// 5. What is the output of this code?
+var i = 0;
+do {
+    console.log(i);
+    i++;
+} while (i < 3);
+// 6. How many times will the following code execute?
+var i = 0;
+do {
+    i++;
+} while (i < 10);
+console.log(i)
+// 7. What will be printed by this code?
+var i = 3;
+while (i > 0) {
+    console.log(i);
+    i--;
+}
+console.clear()
+// 11. How many iterations will the following loop perform?
+var i = 5;
+while (i <= 5) {
+    i++;
+}
+// 12. What will this code print?
+var i = 0;
+for (var j = 0; j < 2; j++) {
+    i++;
+}
+console.log(i);
+// 13. What will be the result of the following code?
+var i = 0;
+do {
+    console.log(i);
+    i++;
+} while (i < 0);
+console.log(i);
+// // 14. What does the following code output?
+for (var i = 0; i < 5; i++) {
+    if (i === 3) continue;
+    console.log(i);
+}
+// 15. How many times will the following loop run?
+var i = 1;
+for (i = 0; i < 10; i++) {
+    if (i == 5) break;
+}
+console.clear()
+// 16. What will the following loop output?
+var i = 0;
+for (var j = 0; j < 2; j++) {
+    i++;
+    console.log(i);
+}
+// 17. What is the output of this code?
+var i = 0;
+while (i < 4) {
+    console.log(i++);
+}
+// 18. What is the result of the following code?
+var i = 1;
+do {
+    console.log(i);
+    i++;
+} while (i <= 5);
+// 19. How many times will the following loop print?
+var i = 0;
+for (i = 0; i < 3; i++) {
+    console.log(i);
+}
+// 20. What will be printed?
+var i = 0;
+do {
+    console.log(i);
+    i++;
+} while (i < 2);
+
+console.clear()
+// 21. What is the result of the following code?
+var i = 3;
+while (i < 5) {
+    i++;
+    console.log(i);
+}
+// 22. What is the output of this code?
+var i = 0;
+for (; i < 4;) {
+    i++;
+    console.log(i);
+}
+// 23. How many times will this loop run?
+var i = 0;
+while (i < 10) {
+    i++;
+    if (i == 5) continue;
+}
+console.log(i)
+// 24. What will the following code output?
+var i = 0;
+do {
+    if (i === 2) break;
+    console.log(i);
+    i++;
+} while (i < 5);
+// 26. How many times will the following loop execute?
+var i = 0;
+do {
+    console.log(i);
+    i++;
+} while (i < 3);
+// 27. What is the output of the following code?
+var i = 2;
+for (i = 0; i < 3; i++) {
+    console.log(i);
+}
+console.clear()
+// 28. What will be printed by this code?
+var i = 0;
+for (var j = 0; j < 3; j++) {
+    console.log(i);
+    i++;
+}
+// 29. What will the following code output?
+var i = 0;
+while (i < 2) {
+    console.log(i);
+    i += 2;
+}
+// 30. What will this code print?
+var i = 0;
+do {
+    if (i === 2) break;
+    console.log(i);
+    i++;
+} while (i < 5);
+// 25. What will be printed by this code?
+for (var i = 0; i < 4; i++) {
+    if (i === 2) continue;
+    console.log(i);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
